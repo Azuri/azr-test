7082cb9f873881b268d7a632b159b351bcf489c8
Todor Kirilov <toscho@gmail.com>
2015-06-04 16:50:28 +0000

    modular test err message added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var log = require('rle/log');
 
 var mongoose = require('mongoose');
 
-//we must load mongoose from lib/models
+// we must load mongoose from lib/models
 //var mongoose = require('rle/models/node_modules/mongoose');
 
 mongoose.connect('mongodb://localhost:27017/test');
@@ -16,7 +16,13 @@ var prod = new Models.Product({
 
 log.info('About to save new product...');
 
+// show error msg after a while
+var t = setTimeout(function(){
+    log.error('... nothing happened!');
+}, 5 * 1000);
+
 prod.save(function(err){
+    clearTimeout(t);
     if (err) {
         return log.error(err);
     }
